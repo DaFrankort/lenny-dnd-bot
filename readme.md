2d0b5e0b48dcb1d80eb2f9ff6ed78fe3c77daf73
Pim Pieters <Pim.Pieters@UGent.be>
2025-02-21 17:02:38 +0100

    Add spell lookup support

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Cloning
+
+This repository can be cloned using
+
+```bash
+git clone --recursive https://github.com/DaFrankort/dndiscord-py.git
+```
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import re
 import discord
 import os
 from dice import Dice
+from spells import Spells, pretty_response_spell
 from discord.ext import commands
 from discord import app_commands
 from dotenv import load_dotenv
@@ -10,6 +11,7 @@ from dotenv import load_dotenv
 # Init
 load_dotenv()
 token = os.getenv("DISCORD_BOT_TOKEN")
+guild_id = int(os.getenv("GUILD_ID"))
 
 intents = discord.Intents.default()
 intents.members = True
@@ -17,6 +19,10 @@ intents.message_content = True
 
 bot = discord.Client(intents=intents)
 cmd_tree = app_commands.CommandTree(bot)
+
+# Dictionaries
+spells = Spells("./submodules/5etools-src/data/spells")
+
 
 # Slash commands
 @cmd_tree.command(
@@ -30,9 +36,10 @@ async def roll(ctx, diceroll: str):
     # if dice is None:
     #     await ctx.response.send_message('⚠️ Format has to be NdN, ex: 1d4. ⚠️', ephemeral=True)
     #     return
-    
+
     result = dice.roll()
     await ctx.response.send_message(result)
+
 
 @cmd_tree.command(
     name="advantage",
@@ -45,6 +52,7 @@ async def advantage(ctx, diceroll: str):
     await ctx.response.send_message(result)
     return
 
+
 @cmd_tree.command(
     name="disadvantage",
     description="Tough luck chump... Roll twice and suck it.",
@@ -56,12 +64,25 @@ async def disadvantage(ctx, diceroll: str):
     await ctx.response.send_message(result)
     return
 
+
+@cmd_tree.command(name="spell", description="Search for a spell.")
+async def search_spell(ctx, name: str):
+    found = spells.search_spell(name)
+    await pretty_response_spell(ctx, found)
+    return
+
+
 # Run
 @bot.event
 async def on_ready():
-    await cmd_tree.sync()
-    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
-    print('------')
+    guild = bot.get_guild(guild_id)
+    if guild is None:
+        print("HELP, CAN'T GET GUILD")
+    else:
+        print(guild.name)
+    await cmd_tree.sync(guild=guild)
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
 
 bot.run(token)
-

ae8047d0dfa89979138e03b5e6de1f033053bc41
ImEldie <daniel.frankort@telenet.be>
2025-05-02 22:51:27 +0200

    Change VoiceChat to VC

--- src/voice_chat.py
+++ src/voice_chat.py
@@ -2,7 +2,7 @@ import logging
 import subprocess
 import discord
 
-class VoiceChat:
+class VC:
     client: discord.VoiceClient
     ffmpeg_available: bool = False
 
@@ -11,7 +11,7 @@ class VoiceChat:
         try:
             subprocess.run(["ffmpeg", "-version"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=True)
             logging.info("FFmpeg installed and available, using voice chat functionality.")
-            VoiceChat.ffmpeg_available = True
+            VC.ffmpeg_available = True
         except (subprocess.CalledProcessError, FileNotFoundError):
             logging.warning("FFmpeg is not installed or not found in PATH, voice chat functionality will be disabled.")
 
@@ -20,34 +20,34 @@ class VoiceChat:
         if ctx.user.voice:
             channel = ctx.user.voice.channel
             if ctx.guild.voice_client is None:
-                VoiceChat.client = await channel.connect()
+                VC.client = await channel.connect()
             else:
-                VoiceChat.client = ctx.guild.voice_client
+                VC.client = ctx.guild.voice_client
 
-            logging.info(f"Joined voicechat: {VoiceChat.client.channel.name}")
+            logging.info(f"Joined VC: {VC.client.channel.name}")
 
     @staticmethod
     async def leave(ctx: discord.Interaction):
-        VoiceChat.client = ctx.guild.voice_client
+        VC.client = ctx.guild.voice_client
         if ctx.voice_client:
             ctx.voice_client.disconnect()
             logging.info("Left the voice channel.")
 
     @staticmethod
     async def play(ctx: discord.Interaction, audio_file: str):
-        if not VoiceChat.ffmpeg_available:
+        if not VC.ffmpeg_available:
             return
 
         if not ctx.guild or not ctx.user.voice:
             return # User in DMs or not in voice chat
         
-        await VoiceChat.join(ctx)
+        await VC.join(ctx)
 
-        if VoiceChat.client.is_playing():
-            VoiceChat.client.stop()
+        if VC.client.is_playing():
+            VC.client.stop()
 
         try:
-            VoiceChat.client.play(discord.FFmpegPCMAudio(source="./sounds/test_sound.mp3"))
+            VC.client.play(discord.FFmpegPCMAudio(source="./sounds/test_sound.mp3"))
         except Exception as e:
             logging.error(f"Error playing audio: {e}")
             return

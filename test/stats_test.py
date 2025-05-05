2d3c88782a4e709f82f3aec612974313b8347971
ImEldie <daniel.frankort@telenet.be>
2025-05-05 20:01:51 +0200

    Add StatsEmbed unit tests

--- test/stats_test.py
+++ test/stats_test.py
@@ -1,13 +1,12 @@
-import pytest
 from stats import StatsEmbed
+from utils.mock_discord_interaction import MockInteraction
 
 class TestStatsEmbed:
     def test_roll(self):
         embed = StatsEmbed(MockInteraction())
         rolls, result = embed._roll()
         
-        assert len(rolls) == 4
-        assert all(1 <= roll <= 6 for roll in rolls)
+        assert len(rolls) == 4, "Should roll 4 dice"
+        assert all(1 <= roll <= 6 for roll in rolls), "Each roll should be between 1 and 6"
         
-        # Check that the result is the sum of the highest 3 rolls
-        assert result == sum(sorted(rolls)[1:])
+        assert result == sum(sorted(rolls)[1:]), "Result should be the sum of the 3 highest rolls"

4022a36666058db60d2aedb480d6fb1f2fe813ce
ImEldie <daniel.frankort@telenet.be>
2025-05-05 19:56:51 +0200

    Init basic Stats unit test

--- test/stats_test.py
+++ test/stats_test.py
@@ -1,1 +1,13 @@
-# INIT
+import pytest
+from stats import StatsEmbed
+
+class TestStatsEmbed:
+    def test_roll(self):
+        embed = StatsEmbed(MockInteraction())
+        rolls, result = embed._roll()
+        
+        assert len(rolls) == 4
+        assert all(1 <= roll <= 6 for roll in rolls)
+        
+        # Check that the result is the sum of the highest 3 rolls
+        assert result == sum(sorted(rolls)[1:])

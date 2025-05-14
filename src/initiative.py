bd71b88a2aef5e8c5d8772b4af46a88f4e3db1a1
DaFrankort <daniel.frankort@telenet.be>
2025-05-14 15:23:40 +0200

    Remove roll() from Initiative

--- src/initiative.py
+++ src/initiative.py
@@ -11,17 +11,12 @@ class Initiative:
     is_npc: bool
 
     def __init__(self, itr: discord.Interaction, modifier: int, name: str | None):
-        self.modifier = modifier
-        self.is_npc = name is not None
-
         if name is None:
             name = itr.user.display_name  # Default to user's name
         self.name = name
-        self.roll()
-
-    def roll(self):
-        """Rolls and sets initiative value"""
         self.d20 = random.randint(1, 20)
+        self.modifier = modifier
+        self.is_npc = name is not None
 
     def get_total(self):
         return self.d20 + self.modifier

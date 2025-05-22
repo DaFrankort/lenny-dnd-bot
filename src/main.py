6bc9e245ba0a8fe8eb37cd3e761e7166f5c485bc
Pim Pieters <pim@pieters.tv>
2025-05-22 15:43:17 +0200

    Add color to logging

--- src/main.py
+++ src/main.py
@@ -1,15 +1,17 @@
 import logging
 import os
 from bot import Bot
-
-logging.basicConfig(
-    level=logging.INFO,  # Set to DEBUG to see all messages, normal is INFO
-    format="%(asctime)s - %(levelname)s - %(message)s",
-    handlers=[logging.StreamHandler()],  # Output to console
-)
-
+from discord.utils import _ColourFormatter
 
 if __name__ == "__main__":
+    # Set up logging using discord.py's _ColourFormatter
+    handler = logging.StreamHandler()
+    handler.setFormatter(_ColourFormatter())
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    logger.addHandler(handler)
+
+    # Start the bot
     os.makedirs("./temp", exist_ok=True)
     bot = Bot()
     bot.run_client()

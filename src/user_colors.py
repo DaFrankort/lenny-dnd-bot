4e10fe7b32f2f1499377bc8e57d222218c253d34
ImEldie <daniel.frankort@telenet.be>
2025-05-22 23:43:04 +0200

    Apply black linting

--- src/user_colors.py
+++ src/user_colors.py
@@ -52,9 +52,9 @@ class UserColor:
 
         while hex_place < 6:
             try:
-                alpha_value = get_alpha(
-                    name[hex_place]
-                ) * get_alpha(name[hex_place + 1])
+                alpha_value = get_alpha(name[hex_place]) * get_alpha(
+                    name[hex_place + 1]
+                )
             except IndexError:
                 # When username is shorter than 6 characters, inserts replacement value.
                 # Value can be changed to 255 for light and blue colors, 0 for dark and red colors.

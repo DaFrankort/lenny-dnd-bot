b64bb821028ae3b691fcda4125103ac5ae49b202
Pim Pieters <pim@pieters.tv>
2025-06-27 11:34:06 +0200

    Add verbose argument

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 import os
 from bot import Bot
@@ -8,12 +9,26 @@ if __name__ == "__main__":
     # Set-up translations
     i18n.set_locale("./assets/locales/en.json")
 
+    # Parse command line arguments, see `python src/main.py --help`
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--verbose",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Enable additional logging. Disabled by default.",
+    )
+    args = parser.parse_args()
+
     # Set up logging using discord.py's _ColourFormatter
     handler = logging.StreamHandler()
     handler.setFormatter(_ColourFormatter())
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logger.addHandler(handler)
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
 
     # Start the bot
     os.makedirs("./temp", exist_ok=True)

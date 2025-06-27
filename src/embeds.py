926d7b24e4361b98e815ba280cbef852c831ea32
ImEldie <daniel.frankort@telenet.be>
2025-06-27 15:31:23 +0200

    Add /language

--- src/embeds.py
+++ src/embeds.py
@@ -9,6 +9,7 @@ from dnd import (
     Description,
     Feat,
     Item,
+    Language,
     Rule,
     Spell,
     Condition,
@@ -467,6 +468,22 @@ class FeatEmbed(_DNDObjectEmbed):
             self.add_field(name="", value=HORIZONTAL_LINE, inline=False)
 
         self.add_description_fields(feat.description)
+
+
+class LanguageEmbed(_DNDObjectEmbed):
+    def __init__(self, language: Language):
+        super().__init__(language)
+        self.description = f"*{language.select_description}*"
+
+        if language.speakers:
+            self.add_field(name="Typical Speakers", value=language.speakers, inline=True)
+        if language.script:
+            self.add_field(name="Script", value=language.script, inline=True)
+
+        if language.description:
+            if len(self.fields) > 0:
+                self.add_field(name="", value=HORIZONTAL_LINE, inline=False)
+            self.add_description_fields(language.description)
 
 
 class SimpleEmbed(discord.Embed):

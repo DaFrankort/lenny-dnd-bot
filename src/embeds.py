449fef630e38461d619fef4e559c90535320f466
ImEldie <daniel.frankort@telenet.be>
2025-06-30 00:59:41 +0200

    Linting

--- src/embeds.py
+++ src/embeds.py
@@ -21,7 +21,9 @@ from rich.console import Console
 HORIZONTAL_LINE = "~~-------------------------------------------------------------------------------------~~"
 
 
-def log_button_press(itr: discord.Interaction, button: discord.ui.Button, location: str):
+def log_button_press(
+    itr: discord.Interaction, button: discord.ui.Button, location: str
+):
     logging.info(f"{itr.user.name} pressed '{button.label}' in {location}")
 
 

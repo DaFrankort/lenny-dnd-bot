d9682847d5be2e16cb897f28eced93868039028b
ImEldie <daniel.frankort@telenet.be>
2025-06-30 00:43:45 +0200

    Set INITIATIVE options to default

--- src/voice_chat.py
+++ src/voice_chat.py
@@ -175,7 +175,6 @@ class Sounds:
             SoundType.ATTACK: option(speed_deviation=0.3),
             SoundType.DAMAGE: option(speed_deviation=0.4),
             SoundType.FIRE: option(speed_deviation=0.2),
-            SoundType.INITIATIVE: option(volume=0.1, speed_deviation=0.2),
             SoundType.PLAYER: option(speed_deviation=0.1),
             SoundType.CREATURE: option(speed_deviation=0.1),
             SoundType.WRITE: option(speed_deviation=0.3),

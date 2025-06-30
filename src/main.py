43d44e7ade9a15fe08039a6a16cd43a1c041704f
ImEldie <daniel.frankort@telenet.be>
2025-06-30 20:05:09 +0200

    Fix flake8 linting error

--- src/main.py
+++ src/main.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
     logger.addHandler(handler)
 
     removeme_list = [1, 2, 3]
-    force_flake8black_error = removeme_list[len(removeme_list) - 1 :]
+    force_flake8black_error = removeme_list[len(removeme_list)-1:]
 
     if args.verbose:
         logger.setLevel(logging.DEBUG)

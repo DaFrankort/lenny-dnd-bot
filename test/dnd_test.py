e0ad4dbf55ba5bff70e1a51410cea92e25c18c44
DaFrankort <daniel.frankort@telenet.be>
2025-07-11 20:32:13 +0200

    Add unit tests for dnddata

---
+++ test/dnd_test.py
@@ -0,0 +1,40 @@
+from dnd import DNDData
+from search import search_from_query
+
+
+class TestDndData:
+    dnd_data = DNDData()
+    queries: list[str] = [
+        "fireball",
+        "dagger",
+        "poisoned",
+        "goblin",
+        "initiative",
+        "attack",
+        "tough",
+        "ABCDF",
+    ]
+
+    def test_dnd_subclass_search(self):
+        for query in self.queries:
+            for data in self.dnd_data:
+                no_error = True
+                try:
+                    data.search(query)
+                except Exception:
+                    no_error = False
+
+                assert (
+                    no_error is True
+                ), f"{data.entries[0].object_type} DNDDataList failed search()"
+
+    def test_search_from_query(self):
+        for query in self.queries:
+            no_error = True
+
+            try:
+                search_from_query(query, self.dnd_data)
+            except Exception:
+                no_error = False
+
+            assert no_error is True, "search_from_query threw an error."

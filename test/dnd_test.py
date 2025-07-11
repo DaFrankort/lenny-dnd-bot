6bb6301629d4b7c06bfaec372f10d3a47c10d861
DaFrankort <daniel.frankort@telenet.be>
2025-07-11 20:39:15 +0200

    Asser false is true in except

--- test/dnd_test.py
+++ test/dnd_test.py
@@ -18,23 +18,16 @@ class TestDndData:
     def test_dnddatalist_search(self):
         for query in self.queries:
             for data in self.dnd_data:
-                no_error = True
                 try:
                     data.search(query)
                 except Exception:
-                    no_error = False
-
-                assert (
-                    no_error
-                ), f"{data.entries[0].object_type} DNDDataList failed search()"
+                    assert (
+                        False is True
+                    ), f"{data.entries[0].object_type} DNDDataList failed search()"
 
     def test_search_from_query(self):
         for query in self.queries:
-            no_error = True
-
             try:
                 search_from_query(query, self.dnd_data)
             except Exception:
-                no_error = False
-
-            assert no_error, "search_from_query threw an error."
+                assert False is True, "search_from_query threw an error."

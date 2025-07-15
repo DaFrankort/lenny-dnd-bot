34ed6a9005aa3f5141244486da25bef99c8f2d5b
DaFrankort <daniel.frankort@telenet.be>
2025-07-15 10:25:11 +0200

    Add argument-list support to cover multiple use-cases

--- test/bot_test.py
+++ test/bot_test.py
@@ -37,26 +37,58 @@ class TestBotCommands:
 
     @pytest.mark.asyncio
     @pytest.mark.parametrize(
-        "cmd_name, args",
+        "cmd_name, arguments",
         [
-            (t("commands.roll.name"), {"diceroll": "1d8+2+1d6", "reason": "Attack"}),
+            (
+                t("commands.roll.name"),
+                [
+                    {"diceroll": "1d8+2+1d6", "reason": None},
+                    {"diceroll": "1d20+5", "reason": "Attack"},
+                    {"diceroll": "Bad Roll", "reason": "1d20"},
+                ],
+            ),
             (t("commands.d20.name"), {}),
-            (t("commands.advantage.name"), {"diceroll": "1d20+5", "reason": "Damage"}),
-            (t("commands.disadvantage.name"), {"diceroll": "4d8", "reason": "Fire"}),
+            (
+                t("commands.advantage.name"),
+                [
+                    {"diceroll": "1d8+2+1d6", "reason": None},
+                    {"diceroll": "1d20+5", "reason": "Attack"},
+                    {"diceroll": "Bad Roll", "reason": "1d20"},
+                ],
+            ),
+            (
+                t("commands.disadvantage.name"),
+                [
+                    {"diceroll": "1d8+2+1d6", "reason": None},
+                    {"diceroll": "1d20+5", "reason": "Attack"},
+                    {"diceroll": "Bad Roll", "reason": "1d20"},
+                ],
+            ),
             (t("commands.shortcut.name"), {}),
-            (t("commands.spell.name"), {"name": "Fire Bolt"}),
-            (t("commands.item.name"), {"name": "Sword"}),
-            (t("commands.condition.name"), {"name": "Poisoned"}),
-            (t("commands.creature.name"), {"name": "Goblin"}),
-            (t("commands.class.name"), {"name": "Wizard"}),
-            (t("commands.rule.name"), {"name": "Action"}),
-            (t("commands.action.name"), {"name": "Attack"}),
-            (t("commands.feat.name"), {"name": "Tough"}),
-            (t("commands.language.name"), {"name": "Common"}),
-            (t("commands.search.name"), {"query": "Barb"}),
-            # /color is ran twice, to clear the useless data it puts inside of user_colors.json
-            (t("commands.color.name"), {"hex_color": "#ff00ff"}),
-            (t("commands.color.name"), {"hex_color": ""}),
+            (t("commands.spell.name"), [{"name": "Fire Bolt"}, {"name": "abcdefg"}]),
+            (t("commands.item.name"), [{"name": "Sword"}, {"name": "abcdefg"}]),
+            (t("commands.condition.name"), [{"name": "Poisoned"}, {"name": "abcdefg"}]),
+            (t("commands.creature.name"), [{"name": "Goblin"}, {"name": "abcdef"}]),
+            (t("commands.class.name"), [{"name": "Wizard"}, {"name": "abcdef"}]),
+            (t("commands.rule.name"), [{"name": "Action"}, {"name": "abcdef"}]),
+            (t("commands.action.name"), [{"name": "Attack"}, {"name": "abcdef"}]),
+            (t("commands.feat.name"), [{"name": "Tough"}, {"name": "abcdef"}]),
+            (t("commands.language.name"), [{"name": "Common"}, {"name": "abcdef"}]),
+            (
+                t("commands.search.name"),
+                [
+                    {"query": "Barb"},
+                    {"query": "qwertyuiopasdfghjkl;zxcvbnm,./1234567890"},
+                ],
+            ),
+            (
+                t("commands.color.name"),
+                [{"hex_color": "#ff00ff"}, {"hex_color": "Not a color"}],
+            ),
+            (
+                t("commands.color.name"),
+                {"hex_color": ""},
+            ),  # Run clear last, to remove useless data from files.
             (t("commands.stats.name"), {}),
             # Generate token commands can't be tested, but generally remain untouched so should rarely break.
             (t("commands.initiative.name"), {}),
@@ -64,13 +96,22 @@ class TestBotCommands:
             # ("", {"": "", "": ""}),
         ],
     )
-    async def test_commands(self, commands, cmd_name, args):
+    async def test_slash_commands(
+        self,
+        commands: list[discord.app_commands.Command],
+        cmd_name: str,
+        arguments: dict | list[dict],
+    ):
         cmd = commands.get(cmd_name)
         assert cmd is not None, f"{cmd_name} command not found"
 
-        try:
-            await cmd.callback(itr=self.mock_interaction, **args)
-        except Exception as e:
-            pytest.fail(
-                f"Error while running command /{cmd_name} with args {args}: {e}"
-            )
+        if isinstance(arguments, dict):
+            arguments = [arguments]  # List required
+
+        for args in arguments:
+            try:
+                await cmd.callback(itr=self.mock_interaction, **args)
+            except Exception as e:
+                pytest.fail(
+                    f"Error while running command /{cmd_name} with args {args}: {e}"
+                )

05d50d249f8364744c625f2e02ed57be2835c584
ImEldie <daniel.frankort@telenet.be>
2025-07-16 22:21:44 +0200

    Add utils.py and refactor polls.py

--- src/polls.py
+++ src/polls.py
@@ -1,6 +1,8 @@
 import datetime
 from discord import Poll
 
+from utils import when
+
 
 class SessionPlanPoll(Poll):
     def __init__(self, in_weeks: int):
@@ -13,40 +15,43 @@ class SessionPlanPoll(Poll):
         self._add_date_answers(in_weeks)
 
     def _get_question(self, in_weeks: int):
-        if in_weeks == 0:
-            return "Session this week, which day works for you?"
-        elif in_weeks == 1:
-            return "Session next week, which day works for you?"
-        return f"Session in {in_weeks} weeks, which day works for you?"
+        if in_weeks in (0, 1):
+            week_phrase = when(in_weeks == 0, "this week", "next week")
+        else:
+            week_phrase = f"in {in_weeks} weeks"
+
+        return f"Session {week_phrase}, which day works for you?"
 
     def _add_date_answers(self, in_weeks: int):
         today = datetime.date.today()
-        if in_weeks == 0:
-            answer_count = (6 - today.weekday()) + 1  # +1 for 'later' option
-        else:
-            answer_count = 7 + 2  # 7 days + earlier/later option
+        is_this_week = in_weeks == 0
+        answer_count_thisweek = (
+            6 - today.weekday() + 1
+        )  # Remaining days of the week + 'later' option
+        answer_count = when(
+            is_this_week, answer_count_thisweek, 9
+        )  # 9 => 7 days + later/earlier options
 
         for i in range(answer_count):
-            relative_text = None
-
-            if i == 0 and in_weeks != 0:
+            if i == 0 and not is_this_week:
                 day_text = "Earlier"
+                relative_text = None
                 emoji = "⬆️"
+
             elif i == answer_count - 1:
                 day_text = "Later"
+                relative_text = None
                 emoji = "⬇️"
-            else:
-                if in_weeks == 0:
-                    day_offset = i + 1  # Start from tomorrow
-                else:
-                    day_offset = (
-                        i - today.weekday() - 1 + (in_weeks * 7)
-                    )  # Shift to monday, with offset to account for 'Earlier' option
 
+            else:
+                shift = i - today.weekday() - 1 + (in_weeks * 7)
+                day_offset = when(is_this_week, i + 1, shift)
                 day = today + datetime.timedelta(days=day_offset)
                 day_text = day.strftime("%A %d %b")
-                relative_text = (
-                    f"In {day_offset} days" if day_offset != 1 else "Tomorrow"
+
+                day_is_tomorrow = day_offset == 1
+                relative_text = when(
+                    day_is_tomorrow, "Tomorrow", f"In {day_offset} days"
                 )
                 emoji = "📅"
 

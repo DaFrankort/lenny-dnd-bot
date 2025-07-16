b5e8efa13c6762c43df31b42bb4f7ae8d7dbc002
DaFrankort <daniel.frankort@telenet.be>
2025-07-16 14:53:11 +0200

    Linting

--- test/help_test.py
+++ test/help_test.py
@@ -11,7 +11,15 @@ class TestHelp:
         command_names = [cmd.name for cmd in bot.tree.get_commands()]
 
         for name in command_names:
-            assert i18n.has(f"commands.{name}.name"), f"No localisation 'name' available for {name}"
-            assert i18n.has(f"commands.{name}.desc"), f"No localisation 'desc' available for {name}"
-            assert i18n.has(f"commands.{name}.help"), f"No localisation 'help' available for {name}"
-            assert i18n.has(f"commands.{name}.command"), f"No localisation 'command' available for {name}"
+            assert i18n.has(
+                f"commands.{name}.name"
+            ), f"No localisation 'name' available for {name}"
+            assert i18n.has(
+                f"commands.{name}.desc"
+            ), f"No localisation 'desc' available for {name}"
+            assert i18n.has(
+                f"commands.{name}.help"
+            ), f"No localisation 'help' available for {name}"
+            assert i18n.has(
+                f"commands.{name}.command"
+            ), f"No localisation 'command' available for {name}"

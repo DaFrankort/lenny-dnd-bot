21206a2c78bd71be316c568b462da0cad358ca41
ImEldie <daniel.frankort@telenet.be>
2025-08-14 00:02:31 +0200

    Add stat sorting by class

--- src/charactergen.py
+++ src/charactergen.py
@@ -1,5 +1,5 @@
 import random
-
+import logging
 import discord
 from dnd import Background, Class, DNDData, DNDTable, Gender
 from embeds import HORIZONTAL_LINE, SimpleEmbed
@@ -13,14 +13,16 @@ class CharacterGen:
     gender: Gender
     character_class: Class
     background: Background
-    stats_raw = Stats
+    stats = Stats
 
     stats_class: list[tuple[str, int]]
     color = None
 
     def __init__(self, itr: discord.Interaction, data: DNDData):
         self.name, self.race, self.gender = data.names.get_random(None, Gender.OTHER)
-        self.character_class = random.choice(data.classes.entries)
+        self.character_class = random.choice(
+            [c_class for c_class in data.classes.entries if c_class.source == "XPHB"]
+        )
         self.background = random.choice(
             [
                 background
@@ -29,7 +31,7 @@ class CharacterGen:
             ]
         )
         self.stats = Stats(itr)
-        # self.sort_stats_for_class(data)
+        self.sort_stats_for_class(data)
 
         self.color = UserColor.get(itr)
 
@@ -37,18 +39,15 @@ class CharacterGen:
         self, data: DNDData
     ):  # TODO FIX BACK END TO HAVE THIS TABLE
         # Get table
+        table_name = "Assign Ability Scores; Standard Array by Class"
         ability_table: DNDTable = None
-        for table in data.tables.get(
-            query="Assign Ability Scores; Standard Array by Class"
-        ):
-            if (
-                r"#assign%20ability%20scores%3b%20standard%20array%20by%20class_xphb"
-                in table.url
-            ):
+        for table in data.tables.get(query=table_name):
+            if table_name.lower() == table.name.lower():
                 ability_table = table
                 break
 
         if ability_table is None:
+            logging.error(f"No table found under {table_name}")
             return None  # TODO: HANDLE
 
         # Get recommended values from table
@@ -68,12 +67,15 @@ class CharacterGen:
                 break
 
         if len(class_stats) != 6:
+            logging.error(
+                f"No 6 class stats found for class {self.character_class.name}"
+            )
             return None  # TODO HANDLE CLASS NOT FOUND
 
         # Sort both stats from largest to smallest
         class_stats = sorted(class_stats, key=lambda x: x[1], reverse=True)
-        stats = [score for _, score in self.stats_class]
-        stats = sorted(stats, key=lambda x: x[1], reverse=True)
+        stats = [score for _, score in self.stats.stats]
+        stats = sorted(stats, reverse=True)
 
         # Overwrite recommended values with rolled values
         self.stats_class = []
@@ -92,9 +94,15 @@ class CharacterGenEmbed(SimpleEmbed):
         desc = f"*{character.gender.value} {character.race} {character.character_class.name}*".title()
         super().__init__(title=character.name, description=None, color=character.color)
         self.description = desc
-        self.add_field(name="", value=HORIZONTAL_LINE)
+
+        stat_text = ""
+        for stat, score in character.stats_class:
+            stat_text += f"- ``{stat}``: {score}\n"
+
+        self.add_field(name="Stats", value=stat_text, inline=True)
         self.add_field(
-            name="",
-            value=f"**Background**: {character.background.name.title()}",
-            inline=False,
+            name="Background",
+            value=character.background.name.title(),
+            inline=True,
         )
+        self.add_field(name="", value=HORIZONTAL_LINE, inline=False)

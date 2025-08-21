59aae71c719b5d07f1c957f0fac60e6d5b53eee2
ImEldie <daniel.frankort@telenet.be>
2025-08-21 23:36:21 +0200

    Remove unused FieldTextDisplay class

--- src/components/items.py
+++ src/components/items.py
@@ -1,6 +1,5 @@
 from discord import ui
 import discord
-from dnd import Description
 from methods import when
 
 
@@ -12,32 +11,6 @@ class TitleTextDisplay(ui.TextDisplay):
         title = when(url, f"[{title}]({url})", title)
         title = f"### {title}"
         super().__init__(content=title, id=id)
-
-
-class FieldTextDisplay(ui.TextDisplay):
-    """A TextDisplay that formats the text as a field, with a label and value."""
-
-    def __init__(self, name: str, value: str, id=None):
-        content = when(name, f"**{name}**\n{value}", value)
-        super().__init__(content=content, id=id)
-
-    @classmethod
-    def from_description(cls, description: Description, id=None):
-        """
-        Build a FieldTextDisplay from a Description object.
-
-        Args:
-            description: An object with 'name' and 'value' attributes.
-            id: Optional ID for the UI element.
-        """
-        name = description["name"]
-        value = description["value"]
-        type = description["type"]
-
-        if type == "table":
-            # TODO BUILD TABLE
-            return cls(name=name, value=value, id=id)
-        return cls(name=name, value=value, id=id)
 
 
 class SimpleSeparator(ui.Separator):

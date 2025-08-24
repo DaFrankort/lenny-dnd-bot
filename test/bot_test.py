a9ddd5b208f9942316881d89cb8f9d9eed57e1c2
ImEldie <daniel.frankort@telenet.be>
2025-08-24 20:48:34 +0200

    Add /stats roll & /stats visualise tests

--- test/bot_test.py
+++ test/bot_test.py
@@ -159,7 +159,11 @@ class TestBotCommands:
             ),
             ("color show", {}),
             ("color clear", {}),  # Run clear last, to remove useless data from files.
-            ("stats", {}),
+            ("stats roll", {}),
+            (
+                "stats visualise",
+                {"str": 10, "dex": 10, "con": 10, "int": 10, "wis": 10, "cha": 10},
+            ),
             (
                 "tokengen",
                 [

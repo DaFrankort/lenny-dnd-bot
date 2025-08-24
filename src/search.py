4a0a8467cf8e22222a5d8d4f13875abd8c122da9
Pim Pieters <pim@pieters.tv>
2025-08-24 12:47:11 +0200

    Add PaginatedLayoutView to search

--- src/search.py
+++ src/search.py
@@ -1,4 +1,3 @@
-import math
 import discord
 from discord import ui
 from rapidfuzz import fuzz
@@ -9,7 +8,7 @@ from dnd import (
     DNDObject,
     send_dnd_embed,
 )
-from modals import SimpleModal
+from ui.paginated_view import PaginatedLayoutView
 
 
 def __search_matches(query: str, name: str, threshold: float) -> bool:
@@ -54,38 +53,17 @@ class SearchSelectButton(ui.Button):
         await send_dnd_embed(itr, self.object)
 
 
-class SearchPageJumpModal(SimpleModal):
-    page: ui.TextInput
-    view: ui.LayoutView
-
-    def __init__(self, itr: discord.Interaction, view: ui.LayoutView):
-        super().__init__(itr=itr, title="Jump pages")
-        self.view = view
-        current_page = str(self.view.page + 1)
-        self.page = ui.TextInput(
-            label=f"Jump to page (1 - {self.view.max_pages})",
-            placeholder=current_page,
-            min_length=1,
-            max_length=len(str(self.view.max_pages)),
-        )
-        self.add_item(self.page)
-
-
-class SearchLayoutView(ui.LayoutView):
-    per_page: int = 10
-
-    page: int
+class SearchLayoutView(PaginatedLayoutView):
     results: DNDSearchResults
 
     container: ui.Container
     title_item: TitleTextDisplay
-    modal: SearchPageJumpModal = None
 
     def __init__(self, query: str, results: DNDSearchResults):
         self.page = 0
         self.results = results
 
-        super().__init__(timeout=None)
+        super().__init__()
 
         title = f"{len(results.get_all())} Results for '{query}'"
         url = f"https://5e.tools/search.html?q={query}"
@@ -94,8 +72,8 @@ class SearchLayoutView(ui.LayoutView):
         self.build()
 
     @property
-    def max_pages(self) -> int:
-        return int(math.ceil(len(self.results) / self.per_page))
+    def entry_count(self) -> int:
+        return len(self.results)
 
     def get_current_options(self):
         start = self.page * self.per_page
@@ -116,80 +94,7 @@ class SearchLayoutView(ui.LayoutView):
             container.add_item(ui.ActionRow(SearchSelectButton(option)))
 
         # FOOTER
-        if self.max_pages > 1:
-            container.add_item(SimpleSeparator())
-            disable_back = self.page == 0
-            disable_next = self.page >= self.max_pages - 1
-            style = discord.ButtonStyle.primary
-
-            button_first_page = ui.Button(label="↞", style=style)
-            button_first_page.callback = lambda itr: self.go_to_first_page(itr)
-            button_first_page.disabled = disable_back
-
-            button_prev_page = ui.Button(label="←", style=style)
-            button_prev_page.callback = lambda itr: self.go_to_prev_page(itr)
-            button_prev_page.disabled = disable_back
-
-            current_page = f"Page {self.page + 1} / {self.max_pages}"
-            button_current_page = ui.Button(
-                label=current_page, style=discord.ButtonStyle.gray
-            )
-            button_current_page.callback = lambda itr: self.jump_to_page_sendmodal(itr)
-
-            button_next_page = ui.Button(label="→", style=style)
-            button_next_page.callback = lambda itr: self.go_to_next_page(itr)
-            button_next_page.disabled = disable_next
-
-            button_last_page = ui.Button(label="↠", style=style)
-            button_last_page.callback = lambda itr: self.go_to_last_page(itr)
-            button_last_page.disabled = disable_next
-
-            container.add_item(
-                ui.ActionRow(
-                    button_first_page,
-                    button_prev_page,
-                    button_current_page,
-                    button_next_page,
-                    button_last_page,
-                )
-            )
+        container.add_item(SimpleSeparator())
+        container.add_item(self.navigation_footer())
 
         self.add_item(container)
-
-    async def rebuild(self, itr: discord.Interaction):
-        self.build()
-        return await itr.response.edit_message(view=self)
-
-    async def go_to_first_page(self, itr: discord.Interaction):
-        self.page = 0
-        return await self.rebuild(itr)
-
-    async def go_to_prev_page(self, itr: discord.Interaction):
-        self.page = self.page - 1
-        return await self.rebuild(itr)
-
-    async def jump_to_page_sendmodal(self, itr: discord.Interaction):
-        self.modal = SearchPageJumpModal(itr, self)
-        self.modal.on_submit = lambda i: self.jump_to_page(i)
-        await itr.response.send_modal(self.modal)
-
-    async def jump_to_page(self, itr: discord.Interaction):
-        page = self.modal.get_int(self.modal.page)
-
-        if page is None:
-            error_message = "❌ Page must be a positive numerical value! ❌"
-            await itr.response.send_message(error_message, ephemeral=True)
-            return
-
-        page -= 1  # First page === 0
-        page = min(max(page, 0), self.max_pages - 1)
-        self.page = page
-        return await self.rebuild(itr)
-
-    async def go_to_next_page(self, itr: discord.Interaction):
-        self.page = self.page + 1
-        return await self.rebuild(itr)
-
-    async def go_to_last_page(self, itr: discord.Interaction):
-        self.page = self.max_pages - 1
-        return await self.rebuild(itr)

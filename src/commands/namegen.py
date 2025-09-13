a6b6cf23b51ab61df0d071e32dbfd42b35b304a6
ImEldie <daniel.frankort@telenet.be>
2025-09-13 13:33:20 +0200

    Refactor /namegen to MVC

---
+++ src/commands/namegen.py
@@ -0,0 +1,51 @@
+import discord
+
+from dnd import Data, Gender
+from embed import SimpleEmbed
+from logic.app_commands import SimpleCommand, send_error_message
+from logic.namegen import NameGenResult, generate_name
+
+
+GenderChoices = [
+    discord.app_commands.Choice(name="Female", value=Gender.FEMALE.value),
+    discord.app_commands.Choice(name="Male", value=Gender.MALE.value),
+    discord.app_commands.Choice(name="Other", value=Gender.OTHER.value),
+]
+
+
+class NameGenCommand(SimpleCommand):
+    name = "namegen"
+    desc = "Generate a random name depending on species and gender!"
+    help = "Get a random name for a humanoid, species and gender can be specified but will default to random values."
+
+    async def species_autocomplete(self, _: discord.Interaction, current: str):
+        species = Data.names.get_species()
+        filtered_species = [
+            spec.title() for spec in species if current.lower() in spec.lower()
+        ]
+        return [
+            discord.app_commands.Choice(name=spec, value=spec)
+            for spec in filtered_species[:25]
+        ]
+
+    @discord.app_commands.describe(
+        species="Request a name from a specific species, selects random species by default.",
+        gender="Request name from a specific gender, selects random gender by default.",
+    )
+    @discord.app_commands.choices(gender=GenderChoices)
+    @discord.app_commands.autocomplete(species=species_autocomplete)
+    async def callback(
+        self,
+        itr: discord.Interaction,
+        species: str = None,
+        gender: str = Gender.OTHER.value,
+    ):
+        self.log(itr)
+        result = generate_name(species, gender)
+        if result is None:
+            await send_error_message(itr, "Can't generate names at this time")
+            return
+        embed = SimpleEmbed(
+            title=result.name, description=result.desc, color=result.color
+        )
+        await itr.response.send_message(embed=embed)
---
+++ src/logic/namegen.py
@@ -0,0 +1,28 @@
+from dnd import Data, Gender
+from user_colors import UserColor
+
+
+class NameGenResult(object):
+    name: str | None
+    desc: str | None
+    color: int | None
+
+    def __init__(self):
+        self.name = None
+        self.desc = None
+        self.color = None
+
+
+def generate_name(species: str | None, gender: str) -> NameGenResult | None:
+    result = NameGenResult()
+
+    gender = Gender(gender)
+    name, new_species, new_gender = Data.names.get_random(species, gender)
+
+    if name and new_species and new_gender:
+        result.name = name
+        result.desc = f"*{new_gender.value} {new_species}*".title()
+        result.color = UserColor.generate(name)
+        return result
+    else:
+        return None

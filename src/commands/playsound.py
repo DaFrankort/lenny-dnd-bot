e7954410931de84e10a68f3fa7f7a8b48d379f06
ImEldie <daniel.frankort@telenet.be>
2025-10-12 10:55:39 +0200

    Add error raising to VC.play_attachment

--- src/commands/playsound.py
+++ src/commands/playsound.py
@@ -1,7 +1,7 @@
 import discord
 
 from logic.app_commands import SimpleCommand
-from embed import SuccessEmbed
+from embed import SimpleEmbed, SuccessEmbed
 from logic.voice_chat import VC
 
 
@@ -12,11 +12,9 @@ class PlaySoundCommand(SimpleCommand):
 
     async def callback(self, itr: discord.Interaction, sound: discord.Attachment):
         self.log(itr)
-        success, description = await VC.play_attachment(itr, sound)
-        embed = SuccessEmbed(
-            title_success="Playing sound!",
-            title_fail=f"Failed to play {sound.filename}...",
-            description=description,
-            success=success,
+        await VC.play_attachment(itr, sound)
+        embed = SimpleEmbed(
+            title="Playing sound!",
+            description=f"▶️ Playing ``{sound.filename}`` in {itr.user.voice.channel.mention}!",
         )
         await itr.response.send_message(embed=embed, ephemeral=True)

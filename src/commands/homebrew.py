c307ded7ec5787370b7ce2e939f8eda1c851fa1f
ImEldie <daniel.frankort@telenet.be>
2025-10-26 11:06:11 +0100

    Add guild-check to prevent /homebrew usage in private messages

--- src/commands/homebrew.py
+++ src/commands/homebrew.py
@@ -1,6 +1,6 @@
 import discord
 from embeds.homebrew import HomebrewEntryAddModal
-from logic.app_commands import SimpleCommand
+from logic.app_commands import SimpleCommand, check_is_guild
 from logic.dnd.abstract import DNDObjectTypes
 
 
@@ -10,6 +10,7 @@ class HomebrewCommand(SimpleCommand):
     help = "Use this command to add, view, or manage homebrew content for your server."
 
     @discord.app_commands.choices(dnd_type=DNDObjectTypes.choices())
+    @discord.app_commands.check(check_is_guild)
     async def callback(self, itr: discord.Interaction, dnd_type: str):
         self.log(itr)
         await itr.response.send_modal(HomebrewEntryAddModal(dnd_type))

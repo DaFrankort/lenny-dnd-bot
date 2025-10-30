347d25c0992a4cfc4fc404d212251e6de91b1b1a
Pim Pieters <pim@pieters.tv>
2025-10-31 00:28:22 +0100

    Add extra charactergen tests

---
+++ test/charactergen_test.py
@@ -0,0 +1,17 @@
+from logic.charactergen import generate_dnd_character
+from logic.dnd.data import Data
+from logic.dnd.name import Gender
+
+
+class TestCharacterGen:
+    def test_all_possible_combinations(self):
+        """Ensures all possible species, class, and gender combinations (in XPHB) are possible."""
+
+        species = [species for species in Data.species.entries if species.source == "XPHB"]
+        classes = [class_ for class_ in Data.classes.entries if class_.source == "XPHB"]
+        genders = Gender.values()
+
+        for spec in species:
+            for class_ in classes:
+                for gender in genders:
+                    generate_dnd_character(gender, spec.name, class_.name)

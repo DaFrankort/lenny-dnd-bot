4aedf63e1b03448e19df85de301ea7b78819217c
ImEldie <daniel.frankort@telenet.be>
2025-11-08 21:12:20 +0100

    Rename _format_md_to_discord

--- src/methods.py
+++ src/methods.py
@@ -39,7 +39,8 @@ class ChoicedEnum(Enum):
         return [e.value for e in cls]
 
 
-def _format_md_to_discord(text: str) -> str:
+def format_markdown_to_discord(text: str) -> str:
+    """Removes markdown formatting that is not compatible with discord."""
     while "####" in text:
         text = text.replace("####", "###")  # unsupported header formats: ### is max header
     text = re.sub(r"\[\[(.*?)\]\]", r"\1", text)  # Obsidian file links: [[FILE]]
@@ -62,5 +63,5 @@ class MDFile:
         file_bytes = await file.read()
         title = file.filename.replace(".md", "").replace("_", " ")
         content = file_bytes.decode("utf-8")
-        content = _format_md_to_discord(content)
+        content = format_markdown_to_discord(content)
         return cls(title, content)

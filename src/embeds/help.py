ab9b3502a64dd17dc82bf76acd196a99442d4e64
DaFrankort <daniel.frankort@telenet.be>
2025-11-10 10:23:45 +0100

    Adjust command-list rendering to support lists larger than 1024 characters

--- src/embeds/help.py
+++ src/embeds/help.py
@@ -99,7 +99,15 @@ class HelpEmbed(discord.Embed):
                 command_desc = self._get_command_desc_line(command)
                 commands_desc.append(command_desc)
 
-        self.add_field(name="", value="\n".join(commands_desc), inline=False)
+        commands_parts: list[str] = []
+        for d in commands_desc:
+            length = len("\n".join(commands_parts)) + len(d)
+            if length > 1024:
+                self.add_field(name="", value="\n".join(commands_parts), inline=False)
+                commands_parts = []
+            commands_parts.append(d)
+        if commands_parts:
+            self.add_field(name="", value="\n".join(commands_parts), inline=False)
 
         # Add extra info
         for info_name, info_fields in tab.info:

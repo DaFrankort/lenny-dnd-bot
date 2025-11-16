a8521fa4f5cbd85e7d0eeceaedb62b2cb830fb09
Pim Pieters <pim@pieters.tv>
2025-11-16 19:13:33 +0100

    Lint logic.timestamp

--- lenny/logic/timestamp.py
+++ lenny/logic/timestamp.py
@@ -40,11 +40,11 @@ def get_relative_timestamp_from_message(message: discord.Message) -> str | None:
         return None
 
     seconds = 0
-    for amount, unit in matches:
-        unit = unit.lower()
+    for amount_raw, unit_raw in matches:
+        unit = unit_raw.lower()
         if unit not in TIME_MULTIPLIERS:
             continue
-        amount = amount.replace(",", ".")
+        amount = amount_raw.replace(",", ".")
         seconds += float(amount) * TIME_MULTIPLIERS[unit]
 
     timestamp = _get_relative_timestamp(message.created_at, seconds)
@@ -61,7 +61,7 @@ def _parse_date_from_string(date: str) -> datetime.date:
         date = f"{day:02d}/{month:02d}/{year}"
 
     elif len(parts) == 2:  # DD/MM
-        day, month = map(int, parts)
+        day, month = [int(part) for part in parts]
         year = discord.utils.utcnow().year
         date = f"{day:02d}/{month:02d}/{year}"
 
@@ -80,13 +80,13 @@ def get_date_timestamp(time: str, timezone: int, date: str | None) -> int:
         date = date.replace(".", "/").strip()
         try:
             base_date = _parse_date_from_string(date)
-        except Exception:
+        except Exception as exc:
             raise SyntaxError(
                 "Date must be in `DD`, `DD/MM`, or `DD/MM/YYYY` format, and must be a valid date!",
-            )
+            ) from exc
 
     time = time.replace(":", "").strip()
-    if not time.isdigit() or not (1 <= len(time) <= 4):
+    if not time.isdigit() or not 1 <= len(time) <= 4:
         raise SyntaxError(
             "Time must be in HHMM or HH format (e.g. `0930`, `15:45`, `700`, or `7`).",
         )
@@ -98,8 +98,8 @@ def get_date_timestamp(time: str, timezone: int, date: str | None) -> int:
     hours, minutes = divmod(int(time), 100)
     try:
         dt = datetime.datetime.combine(base_date, datetime.time(hour=hours, minute=minutes))
-    except ValueError as e:
-        raise ValueError(f"Invalid time: {str(e)}")
+    except ValueError as exc:
+        raise ValueError(f"Invalid time: {str(exc)}") from exc
 
     dt_utc = dt - datetime.timedelta(hours=timezone)  # Adjust for timezone
     unix_timestamp = int(dt_utc.replace(tzinfo=datetime.timezone.utc).timestamp())

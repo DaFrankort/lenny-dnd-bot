8127d21360b70e2035ffb24ed1c52a7cd237f17e
Pim Pieters <pim@pieters.tv>
2026-01-01 13:06:06 +0100

    Move cult description up

--- lenny/embeds/dnd/cult.py
+++ lenny/embeds/dnd/cult.py
@@ -8,12 +8,12 @@ class CultEmbed(DNDEntryEmbed):
 
         self.add_field(name="", value=f"*{cult.type} cult*", inline=False)
 
+        if cult.description:
+            self.add_description_fields(cult.description)
+
         if cult.goal:
             self.add_field(name="Goal", value=cult.goal, inline=False)
         if cult.cultists:
             self.add_field(name="Cultists", value=cult.cultists, inline=False)
         if cult.signatureSpells:
             self.add_field(name="Spells", value=cult.signatureSpells, inline=False)
-
-        if cult.description:
-            self.add_description_fields(cult.description)

543f7b721f95a82aaf7b18bf323d54b68540f99b
Pim Pieters <pim@pieters.tv>
2026-01-03 14:32:07 +0100

    Rework background

--- lenny/logic/dnd/background.py
+++ lenny/logic/dnd/background.py
@@ -5,7 +5,14 @@ from logic.dnd.abstract import Description, DNDEntry, DNDEntryList, DNDEntryType
 
 class Background(DNDEntry):
     abilities: list[str]
+    feat: str | None
+    skills: str | None
+    tools: str | None
+    languages: str | None
+    equipment: str | None
+    prerequisite: str | None
     description: list[Description]
+    fluff: list[Description]
 
     def __init__(self, obj: dict[str, Any]):
         self.entry_type = DNDEntryType.BACKGROUND
@@ -13,8 +20,16 @@ class Background(DNDEntry):
         self.name = obj["name"]
         self.source = obj["source"]
         self.url = obj["url"]
+
         self.abilities = obj["abilities"] or []
+        self.feat = obj["feat"]
+        self.skills = obj["skills"]
+        self.tools = obj["tools"]
+        self.languages = obj["languages"]
+        self.equipment = obj["equipment"]
+        self.prerequisite = obj["prerequisite"]
         self.description = obj["description"]
+        self.fluff = obj["fluff"]
 
 
 class BackgroundList(DNDEntryList[Background]):

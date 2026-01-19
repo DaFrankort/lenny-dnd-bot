3d13b305e1dea331c59b31033446151a23ee01e0
DaFrankort <daniel.frankort@telenet.be>
2026-01-19 09:42:16 +0100

    Add new creature data to Creature Entries

--- lenny/logic/dnd/creature.py
+++ lenny/logic/dnd/creature.py
@@ -6,8 +6,14 @@ from logic.dnd.abstract import Description, DNDEntry, DNDEntryList, DNDEntryType
 class Creature(DNDEntry):
     subtitle: str | None
     summoned_by_spell: str | None
+    summoned_by_class: str | None
     token_url: str | None
     description: list[Description]
+    stats: Description
+    details: Description
+    traits: list[Description]
+    actions: list[Description]
+    bonus_actions: list[Description]
 
     def __init__(self, obj: dict[str, Any]):
         self.entry_type = DNDEntryType.CREATURE
@@ -18,10 +24,23 @@ class Creature(DNDEntry):
 
         self.subtitle = obj["subtitle"]
         self.summoned_by_spell = obj["summonedBySpell"]
+        self.summoned_by_class = obj["summonedByClass"]
         self.token_url = obj["tokenUrl"]
-        self.description = obj["description"]
 
+        self.description = obj["description"]
         self.select_description = self.subtitle
+
+        # Currently only summonable creatures require this data.
+        self.stats = obj["stats"] if self.is_summonable else []  # type: ignore
+        self.details = obj["details"] if self.is_summonable else []  # type: ignore
+        self.traits = obj["traits"] if self.is_summonable else []  # type: ignore
+        self.actions = obj["actions"] if self.is_summonable else []  # type: ignore
+        self.bonus_actions = obj["bonusActions"] if self.is_summonable else []  # type: ignore
+
+    @property
+    def is_summonable(self):
+        # TODO Only show if summonable for class, or also for spell?
+        return self.summoned_by_class is not None
 
 
 class CreatureList(DNDEntryList[Creature]):

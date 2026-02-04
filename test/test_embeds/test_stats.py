320c24a23aad5a53759c285c6da653583258a8b4
Pim Pieters <pim@pieters.tv>
2026-02-05 00:08:52 +0100

    Move stats tests

---
+++ test/test_embeds/test_stats.py
@@ -0,0 +1,33 @@
+import discord
+import pytest
+from embeds.stats import StatsEmbed
+from logic.stats import Stats
+from utils.mocking import MockInteraction
+
+
+class TestStats:
+    @pytest.fixture()
+    def itr(self):
+        return MockInteraction()
+
+    @pytest.fixture()
+    def stats(self):
+        return Stats()
+
+    def test_get_embed_title(self, itr: discord.Interaction, stats: Stats):
+        embed = StatsEmbed(itr, stats)
+        embed_title = embed.get_title()
+
+        assert itr.user.display_name in embed_title, "Embed title should contain the user's display name"
+
+    def test_get_embed_description(self, itr: discord.Interaction, stats: Stats):
+        embed = StatsEmbed(itr, stats)
+        description = embed.get_description()
+
+        for rolls, result in stats.stats:
+            for index, roll in enumerate(rolls):
+                assert str(roll) in description, f"Description should contain the roll {roll} at index {index}"
+            assert str(result) in description, f"Description should contain the result {result}"
+
+        total = sum(result for _, result in stats.stats)
+        assert str(total) in description, "Description should contain the total of all stats"
---
+++ test/test_logic/test_stats.py
@@ -0,0 +1,19 @@
+import pytest
+from logic.stats import Stats
+
+
+class TestStats:
+    def test_stat_values(self):
+        stats = Stats()
+
+        for stat in stats.stats:
+            rolls, result = stat
+            assert len(rolls) == 4, "There should be 4 rolls"
+            assert all(1 <= roll <= 6 for roll in rolls), "Rolls should be between 1 and 6"
+            assert isinstance(result, int), "Result should be an integer"
+            assert 3 <= result <= 24, "Result should be between 4 and 24 (sum of 3 rolls)"
+
+    def test_stat_limits(self):
+        """Stats can only roll up to 108 total. In this case, the value will never be reached and will thus timeout."""
+        with pytest.raises(ValueError):
+            Stats(min_total=999)

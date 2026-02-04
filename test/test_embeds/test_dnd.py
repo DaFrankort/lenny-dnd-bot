f11aa8836b169d707513d1ef8cc1c6364f5e64c6
Pim Pieters <pim@pieters.tv>
2026-02-05 00:21:13 +0100

    Move D&D data tests

---
+++ test/test_embeds/test_dnd.py
@@ -0,0 +1,25 @@
+import discord
+import pytest
+
+from embeds.search import MultiDNDSelectView
+from logic.config import Config
+from logic.dnd.data import Data
+from utils.mocking import MockInteraction
+
+
+class TestDNDEmbed:
+    @pytest.fixture()
+    def itr(self):
+        return MockInteraction()
+
+    async def test_multidndselect(self, itr: discord.Interaction):
+        itr = MockInteraction()
+        config = Config.get(itr)
+        sources = config.allowed_sources
+        name = "pot of awakening"
+        entries = Data.items.get(name, sources)
+        assert len(entries) >= 2, "Test requires at least 2 items, please update test data."
+        try:
+            MultiDNDSelectView(name, entries)
+        except Exception as e:
+            pytest.fail(f"MultiDNDSelectView failed to initialize: {e}")
--- test/test_logic/test_dnd.py
+++ test/test_logic/test_dnd.py
@@ -2,13 +2,12 @@ import pytest
 from utils.mocking import MockInteraction
 
 
-from embeds.search import MultiDNDSelectView
 from logic.config import Config
 from logic.dnd.abstract import fuzzy_matches
 from logic.dnd.data import Data
 
 
-class TestDndData:
+class TestDNDData:
     queries: list[str] = [
         "fireball",
         "dagger",
@@ -40,19 +39,6 @@ class TestDndData:
                 Data.search(query, allowed_sources=sources)
             except Exception:
                 assert False, "search_from_query threw an error."
-
-    @pytest.mark.asyncio
-    async def test_multidndselect(self):
-        itr = MockInteraction()
-        config = Config.get(itr)
-        sources = config.allowed_sources
-        name = "pot of awakening"
-        entries = Data.items.get(name, sources)
-        assert len(entries) >= 2, "Test requires at least 2 items, please update test data."
-        try:
-            MultiDNDSelectView(name, entries)
-        except Exception as e:
-            pytest.fail(f"MultiDNDSelectView failed to initialize: {e}")
 
     @pytest.mark.parametrize(
         "query, value, result",

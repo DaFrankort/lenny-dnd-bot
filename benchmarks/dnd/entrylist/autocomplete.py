44bf9ac98b28ac7c5a36722c9b0c15e830f6b097
DaFrankort <daniel.frankort@telenet.be>
2026-02-18 11:51:57 +0100

    Add dnd entrylist benchmarks

---
+++ benchmarks/dnd/entrylist/autocomplete.py
@@ -0,0 +1,27 @@
+from benchmarks.dnd.utils import parametrize_entry_lists
+from pytest_benchmark.fixture import BenchmarkFixture
+
+from logic.dnd.abstract import DNDEntry, DNDEntryList
+from logic.dnd.source import ContentChoice, SourceList
+
+
+@parametrize_entry_lists()
+def test_dnd_entry_list_autocomplete(benchmark: BenchmarkFixture, entry_list: DNDEntryList[DNDEntry]) -> None:
+    names: list[str] = [e.name for e in entry_list.entries]
+    sources: set[str] = set({e.name for e in SourceList(content=ContentChoice.ALL).entries})
+    index = 0
+
+    def setup() -> tuple[tuple[str], dict[str, set[str]]]:
+        nonlocal index
+        name = names[index]
+        index = (index + 1) % len(names)
+        return (
+            (name,),
+            {"allowed_sources": sources},
+        )
+
+    benchmark.pedantic(  # type: ignore
+        target=entry_list.get_autocomplete_suggestions,
+        setup=setup,
+        rounds=max(len(names), 256),
+    )

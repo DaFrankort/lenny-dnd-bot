d576d05964b6cc2df882fd6dca5b331632283627
Pim Pieters <pim@pieters.tv>
2026-02-23 01:04:00 +0100

    Remove code duplication for D&D entry list benchmarks

--- benchmarks/dnd/dnd_utils.py
+++ benchmarks/dnd/dnd_utils.py
@@ -1,6 +1,9 @@
 import pytest
+from pytest_benchmark.fixture import BenchmarkFixture
 
+from logic.dnd.abstract import DNDEntry, DNDEntryList
 from logic.dnd.data import Data
+from logic.dnd.source import ContentChoice, SourceList
 
 ENTRY_LIST_PARAMS = [
     ("actions", Data.actions),
@@ -30,4 +33,26 @@ def parametrize_entry_lists():
         "entry_list",
         [v for _, v in ENTRY_LIST_PARAMS],
         ids=[k for k, _ in ENTRY_LIST_PARAMS],
+    )
+
+
+@parametrize_entry_lists()
+def benchmark_entry_list(benchmark: BenchmarkFixture, entry_list: DNDEntryList[DNDEntry], method: str) -> None:
+    names: list[str] = [e.name for e in entry_list.entries]
+    sources: set[str] = set({e.name for e in SourceList(content=ContentChoice.ALL).entries})
+    index = 0
+
+    def setup() -> tuple[tuple[str], dict[str, set[str]]]:
+        nonlocal index
+        name = names[index]
+        index = (index + 1) % len(names)
+        return (
+            (name,),
+            {"allowed_sources": sources},
+        )
+
+    benchmark.pedantic(  # type: ignore
+        target=getattr(entry_list, method),
+        setup=setup,
+        rounds=max(len(names), 256),
     )

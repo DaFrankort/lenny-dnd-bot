69cb8f26bdda2b66360d5691319d7bc148a0b6de
ImEldie <daniel.frankort@telenet.be>
2026-03-09 19:07:34 +0100

    Move initialization of stats to __init__

--- lenny/logic/stats.py
+++ lenny/logic/stats.py
@@ -46,13 +46,14 @@ class Stats:
 
 
 class BoughtStats:
-    stats: dict[str, int] = {"STR": 8, "DEX": 8, "CON": 8, "INT": 8, "WIS": 8, "CHA": 8}
+    stats: dict[str, int]
     max_points: int
     owner_id: int
 
     def __init__(self, itr: discord.Interaction):
         self.max_points = 27
         self.owner_id = itr.user.id
+        self.stats = {"STR": 8, "DEX": 8, "CON": 8, "INT": 8, "WIS": 8, "CHA": 8}
 
     @property
     def spent(self) -> int:

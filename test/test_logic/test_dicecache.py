8308db4f89704e56608ee1a92ef4c8085116f78e
ImEldie <daniel.frankort@telenet.be>
2026-04-29 20:40:20 +0200

    pylint

--- test/test_logic/test_dicecache.py
+++ test/test_logic/test_dicecache.py
@@ -31,12 +31,12 @@ class TestDiceExpressionCache:
         assert reason in data.reasons, f"'{reason} should be in 'reasons'"
 
     def test_get_autocomplete_suggestions_empty(self, itr: Interaction):
-        DiceCache.get(itr).cache = DiceCacheInfo([], [], 0)
+        DiceCache.get(itr).cache = DiceCacheInfo([], [], 0, {})
         suggestions = DiceCache.get(itr).get_autocomplete_suggestions("")
         assert suggestions == [], "Suggestions should be empty when no data is present."
 
     def test_autocompletes_clean_dice_instead_of_cache(self, itr: Interaction):
-        DiceCache.get(itr).cache = DiceCacheInfo([], [], 0)
+        DiceCache.get(itr).cache = DiceCacheInfo([], [], 0, {})
         expected = "1d20"
         cached_expression = f"{expected}+5"
         DiceCache.get(itr).store_expression(cached_expression)

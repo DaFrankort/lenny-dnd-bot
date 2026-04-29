1ccfabf790006c9b73ecee326e327a694e2dbc48
DaFrankort <daniel.frankort@telenet.be>
2026-04-29 17:34:46 +0200

    Make dice-trie per-user rather than global

--- lenny/logic/dicecache.py
+++ lenny/logic/dicecache.py
@@ -3,12 +3,22 @@ from dataclasses import dataclass
 from typing import Any
 
 import discord
+import pygtrie  # pyright: ignore[reportMissingTypeStubs]
 from discord.app_commands import Choice
 
-from logic.dicetriecache import DiceTrieCache
 from logic.dnd.data import Data
 from logic.jsonhandler import JsonFolderHandler, JsonHandler
 from logic.voice_chat import SPECIAL_ROLL_REASONS
+
+DEFAULT_TRIE = {
+    "1d10red": 5,  # Cyberpunk Red roll
+    "1d8e8": 5,  # Sorcerous Burst
+    "4d6kh3": 5,  # Stat rolling
+    "2d20kh1": 10,  # Advantage
+    "2d20kl1": 10,  # Disadvantage
+    "2d4+2": 10,  # Potion of Healing
+    "4d4+4": 10,  # Potion of Greater Healing
+}
 
 
 @dataclass
@@ -16,21 +26,80 @@ class DiceCacheInfo:
     rolls: list[str]
     reasons: list[str]
     initiative: int
+    trie: dict[str, int]
 
     @classmethod
     def fromdict(cls, obj: Any) -> "DiceCacheInfo":
-        return cls(
-            rolls=obj.get("rolls", []),
-            reasons=obj.get("reasons", []),
-            initiative=obj.get("initiative", 0),
-        )
+        return cls(rolls=obj.get("rolls", []), reasons=obj.get("reasons", []), initiative=obj.get("initiative", 0), trie={})
+
+
+def normalize_expression(expression: str) -> str:
+    # TODO Sorting dice expressions and merging compatible dice and modifiers could be beneficial.
+    # e.g. 1d8+1d6+2d6+1+4 => 1d8+3d6+5
+    return expression.strip().lower().replace(" ", "")
+
+
+class DiceCacheTrie:
+    _data: DiceCacheInfo
+
+    def __init__(self, data: DiceCacheInfo):
+        self._trie = pygtrie.CharTrie(data.trie)
+        self._data = data
+        if len(self._data.trie) == 0:
+            self._data.trie = DEFAULT_TRIE
+
+    def add(self, expression: str):
+        expression = normalize_expression(expression)
+        if not expression:
+            return
+
+        count: int = self._trie.get(expression, 0)  # type: ignore
+        self._trie[expression] = count + 1
+        self._data.trie = dict(self._trie.items())  # type: ignore
+
+    def get_suggestions(self, expression: str, limit: int = 25) -> list[str]:
+        expression = normalize_expression(expression)
+        if not expression:
+            return []
+
+        try:
+            matches = list(self._trie.items(prefix=expression))  # type: ignore
+            matches.sort(key=lambda item: item[1], reverse=True)  # type: ignore
+            return [expression for expression, _ in matches[:limit]]  # type: ignore
+        except KeyError:
+            return []
+
+    def clean(self, limit: int = 50, max_count: int = 100):
+        items = list(self._trie.items())  # type: ignore
+        if len(items) <= limit:  # type: ignore
+            return
+
+        # If an expression has thousands of uses, it will just claim space and will
+        # likely never be removed from the trie, removing newer expressions instead.
+        # To make it easier for a user to 'clear' an expression from the trie, we
+        # limit the max count and halve all counts if that limit is exceeded.
+        if items and max(count for _, count in items) >= max_count:  # type: ignore
+            for key in self._trie.keys():  # type: ignore
+                self._trie[key] = max(1, self._trie[key] // 2)  # type: ignore
+            items = list(self._trie.items())  # type: ignore
+
+        # Remove least used counts
+        items.sort(key=lambda x: x[1], reverse=True)  # type: ignore
+        keys_to_remove = [expr for expr, _ in items[limit:]]  # type: ignore
+        for key in keys_to_remove:  # type: ignore
+            del self._trie[key]
+
+        self._data.trie = dict(self._trie.items())  # type: ignore
 
 
 class DiceCacheHandler(JsonHandler[DiceCacheInfo]):
+    _trie: DiceCacheTrie
+
     def __init__(self, user_id: int):
         super().__init__(str(user_id), "user_cache")
         if not self.data:
-            self.cache = DiceCacheInfo(rolls=[], reasons=[], initiative=0)
+            self.cache = DiceCacheInfo(rolls=[], reasons=[], initiative=0, trie={})  # TODO init trie data
+        self._trie = DiceCacheTrie(self.cache)
 
     @property
     def cache(self) -> DiceCacheInfo:
@@ -50,7 +119,7 @@ class DiceCacheHandler(JsonHandler[DiceCacheInfo]):
         self.cache.rolls.append(expression)
 
         self.cache.rolls = self.cache.rolls[-5:]  # Store max 5 expressions
-        DiceTrieCache.add(expression)
+        self._trie.add(expression)
         self.save()
 
     def store_reason(self, reason: str | None):
@@ -99,7 +168,7 @@ class DiceCacheHandler(JsonHandler[DiceCacheInfo]):
             if query in roll.lower():
                 add_suggestion(roll)
 
-        for roll in DiceTrieCache.get_suggestions(query, 5):
+        for roll in self._trie.get_suggestions(query, 5):
             add_suggestion(roll)
 
         return [Choice(name=roll, value=roll) for roll in suggestions[:5]]

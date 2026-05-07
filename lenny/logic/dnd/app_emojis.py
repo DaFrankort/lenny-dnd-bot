9715432365cfd694e6541107dafaae89ed91fc4a
DaFrankort <daniel.frankort@telenet.be>
2026-05-07 16:36:11 +0200

    Move DNDEntry emojis to AppEmoji

--- lenny/logic/dnd/app_emojis.py
+++ lenny/logic/dnd/app_emojis.py
@@ -22,10 +22,51 @@ def init_app_emojis(emojis: list[discord.Emoji]):
 class AppEmoji(Enum):
     TEST = "dsa"
 
+    # ENTRY TYPES
+    ACTION = "action"
+    BACKGROUND = "background"
+    CLASS = "class"
+    CONDITION = "condition"
+    CREATURE = "creature"
+    DEITY = "deity"
+    FEAT = "feat"
+    HAZARD = "hazard"
+    ITEM = "item"
+    LANGUAGE = "language"
+    OBJECT = "object"
+    RULE = "rule"
+    SPECIES = "species"
+    SPELL = "spell"
+    TABLE = "table"
+    VEHICLE = "vehicle"
+    CULT = "cult"
+    BOON = "boon"
+    SKILL = "skill"
+
     @property
     def _fallback(self) -> str:
         fallback_map = {
             self.TEST: "🎲",
+            # Entry Types
+            self.ACTION: "🏃",
+            self.BACKGROUND: "📕",
+            self.CLASS: "🧙‍♂️",
+            self.CONDITION: "🤒",
+            self.CREATURE: "🐉",
+            self.DEITY: "👁️",
+            self.FEAT: "🎖️",
+            self.HAZARD: "🪤",
+            self.ITEM: "🗡️",
+            self.LANGUAGE: "💬",
+            self.OBJECT: "🪨",
+            self.RULE: "📜",
+            self.SPECIES: "🧝",
+            self.SPELL: "🔥",
+            self.TABLE: "📊",
+            self.VEHICLE: "⛵",
+            self.CULT: "🕯️",
+            self.BOON: "🎁",
+            self.SKILL: "🎯",
         }
         logging.warning(f"Used fallback emoji for '{self.value}'")
         return fallback_map.get(self, "❓")

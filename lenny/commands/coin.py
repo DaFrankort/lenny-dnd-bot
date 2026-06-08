ce9b5421df4422b26fa69bd81f53e69003b77230
DaFrankort <daniel.frankort@telenet.be>
2026-06-08 11:15:11 +0200

    Add coin expression description

--- lenny/commands/coin.py
+++ lenny/commands/coin.py
@@ -1,6 +1,7 @@
 import discord
 
 from commands.command import BaseCommand
+from discord.app_commands import describe
 from embeds.embed import UserActionEmbed
 from logic.coin import Coin
 
@@ -10,6 +11,7 @@ class CoinCommand(BaseCommand):
     desc = "Perform D&D currency math!"
     help = "Calculate your pieces using addition, subtraction, division and multiplication!"
 
+    @describe(expression="Units: cp, sp, ep, gp, pp; Operators: + - * /")
     async def handle(self, itr: discord.Interaction, expression: str):
         coin = Coin.from_string(expression)
         embed = UserActionEmbed(itr, title=expression.lower(), description=str(coin))

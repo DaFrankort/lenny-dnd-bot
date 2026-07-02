037da268f4825a51c10e5c487448a201ddeab5a0
DaFrankort <daniel.frankort@telenet.be>
2026-07-02 16:40:54 +0200

    Add weight to SessionTitles & Add titles for advantage and disadvantage

--- lenny/logic/session/titles.py
+++ lenny/logic/session/titles.py
@@ -6,14 +6,26 @@ from logic.session.types import UserSessionDiceStats, UserSessionStats
 class SessionTitle(ABC):
     name: str
     description: str
+    weight: float = 1.0
 
     @abstractmethod
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+        """
+        Calculates an unweighted score to represent how well the user qualifies for this title.
+        Do not use the title's weight in evaluation, this is applied in the main evaluate() method.
+        """
+
+        raise NotImplementedError(f"The title '{self.name}' can not be evaluated yet.")
+
     def evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
         """
-        Returns a score representing how well the player qualifies for this title.
-        Return 0 or negative if they don't qualify at all.
+        Returns weighted score representing how well the user qualifies for this title.
+        Returns 0 or negative if they don't qualify at all.
+
+        A title's weight can be adjusted to finetune how easy it is to receive this title.
         """
-        raise NotImplementedError(f"The title '{self.name}' can not be evaluated yet.")
+
+        return self._evaluate(stats, all_session_stats) * self.weight
 
 
 class TitleRegistry:
@@ -27,6 +39,8 @@ class TitleRegistry:
             TitleConsistentRoller(),
             TitleHeavyHitter(),
             TitleWeakHitter(),
+            TitleAdvantage(),
+            TitleDisadvantage(),
         ]
         self.assigned = set()
 
@@ -51,10 +65,11 @@ class TitleRegistry:
 # TITLES
 # d20 check related titles
 class TitleMostNat1(SessionTitle):
-    name = "Bad Dice Day"
+    name = "The Murphy's Law Enthusiast"
     description = "Rolled the most Natural 1s this session."
+    weight = 5.0
 
-    def evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
         if stats.nat1_count == 0:
             return 0
 
@@ -65,10 +80,11 @@ class TitleMostNat1(SessionTitle):
 
 
 class TitleMostNat20(SessionTitle):
-    name = "Lucky Hand"
+    name = "The Weighted Dice User"
     description = "Rolled the most Natural 20s this session."
+    weight = 5.0
 
-    def evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
         if stats.nat20_count == 0:
             return 0
 
@@ -79,10 +95,10 @@ class TitleMostNat20(SessionTitle):
 
 
 class TitleConsistentRoller(SessionTitle):
-    name = "Reliable"
+    name = "The Ol' Reliable"
     description = "Maintained an average d20 roll of 14 or higher."
 
-    def evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
         # Require at least 5 rolls, to prevent flukes.
         if len(stats.d20_totals) >= 5 and stats.average_d20 >= 14:
             return float(stats.average_d20)
@@ -91,10 +107,11 @@ class TitleConsistentRoller(SessionTitle):
 
 # Damage related titles
 class TitleHeavyHitter(SessionTitle):
-    name = "The Juggernaut"
+    name = "The Min-Maxxer"
     description = "Dealt the highest total damage across the entire session."
+    weight = 0.7
 
-    def evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
         user_total_dmg = sum(stats.damage_totals)
         if user_total_dmg == 0:
             return 0
@@ -106,10 +123,11 @@ class TitleHeavyHitter(SessionTitle):
 
 
 class TitleWeakHitter(SessionTitle):
-    name = "Suboptimal Build"
+    name = "The Emotional Support"
     description = "Dealt the least amount of damage across the entire session."
+    weight = 5.0
 
-    def evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
         user_total_dmg = sum(stats.damage_totals)
         if user_total_dmg == 0:
             return 0
@@ -117,4 +135,37 @@ class TitleWeakHitter(SessionTitle):
         min_dmg = min(sum(s.dice.damage_totals) for s in all_session_stats.values())
         if user_total_dmg == min_dmg:
             return float(user_total_dmg)
+        return 0
+
+
+# Advantage User
+class TitleAdvantage(SessionTitle):
+    name = "The Crit Fisher"
+    description = "Rolled with advantage more times than anyone else."
+    weight = 2.0
+
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+        adv_count = stats.adv_count
+        if adv_count == 0:
+            return 0
+
+        max_count = max(s.dice.adv_count for s in all_session_stats.values())
+        if adv_count == max_count:
+            return float(adv_count)
+        return 0
+
+
+class TitleDisadvantage(SessionTitle):
+    name = "The DM's Least Favorite"
+    description = "Rolled with disadvantage more times than anyone else."
+    weight = 5.0
+
+    def _evaluate(self, stats: UserSessionDiceStats, all_session_stats: dict[int, UserSessionStats]) -> float:
+        dis_count = stats.dis_count
+        if dis_count == 0:
+            return 0
+
+        max_count = max(s.dice.dis_count for s in all_session_stats.values())
+        if dis_count == max_count:
+            return float(dis_count)
         return 0

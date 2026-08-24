a2ad51a3707060879694c3872dc26c00b8196dcc
DaFrankort <daniel.frankort@telenet.be>
2026-08-24 16:01:03 +0200

    Adjust OptionalFeatureEmbed to match FeatEmbed styling

--- lenny/embeds/dnd/optionalfeature.py
+++ lenny/embeds/dnd/optionalfeature.py
@@ -1,4 +1,4 @@
-from embeds.dnd.abstract import DNDEntryEmbed
+from embeds.dnd.abstract import HORIZONTAL_LINE, DNDEntryEmbed
 from logic.dnd.optionalfeature import OptionalFeature
 
 
@@ -6,8 +6,10 @@ class OptionalFeatureEmbed(DNDEntryEmbed):
 
     def __init__(self, optional_feat: OptionalFeature):
         super().__init__(entry=optional_feat)
+        self.description = f"*{optional_feat.type}*"
 
         if optional_feat.prerequisite:
-            self.add_field(name="Prerequisite", value=optional_feat.prerequisite, inline=True)
-        self.add_field(name="Type", value=optional_feat.type, inline=True)
-        self.add_description_fields(optional_feat.description)
+            self.add_field(name="Requires", value=optional_feat.prerequisite)
+        if len(optional_feat.description) > 0:
+            self.add_field(name="", value=HORIZONTAL_LINE, inline=False)
+            self.add_description_fields(optional_feat.description)

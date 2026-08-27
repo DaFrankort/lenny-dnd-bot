06e4140a4618b7056986dfbd098697588ada2ffa
DaFrankort <daniel.frankort@telenet.be>
2026-08-27 12:14:31 +0200

    Linting & isort

--- test/test_embeds/test_help.py
+++ test/test_embeds/test_help.py
@@ -1,7 +1,7 @@
-from test.mocking import MockBot
-
 import pytest
 from assertion import assert_embed_can_be_rendered
+from bot import Bot
+from mocking import MockBot
 
 from embeds.help import HelpEmbed
 from logic.help import HelpTabs
@@ -10,6 +10,6 @@ from logic.help import HelpTabs
 class TestHelp:
     @pytest.mark.parametrize("tab", HelpTabs.keys)
     def test_tab_rendering(self, tab: str):
-        bot = MockBot()
+        bot: Bot = MockBot()
         embed = HelpEmbed(bot.tree, tab=tab)
         assert_embed_can_be_rendered(embed, tab)

782310a923f581bf5dc097eb8dc4fa92d8892feb
ImEldie <daniel.frankort@telenet.be>
2026-09-04 20:36:46 +0200

    Fix condition rendering

--- lenny/embeds/dnd/condition.py
+++ lenny/embeds/dnd/condition.py
@@ -6,15 +6,8 @@ class ConditionEmbed(DNDEntryEmbed):
     def __init__(self, condition: Condition):
         super().__init__(condition)
 
-        if len(condition.description) == 0:
-            return
-
-        description = condition.description[0]
-        if description["type"] != "text":
-            raise TypeError(f"Condition description should be a string, but it is a {description['type']}!")
-
-        self.description = description["value"]
-        self.add_description_fields(condition.description[1:])
-
         if condition.image:
             self.set_thumbnail(url=condition.image)
+
+        if len(condition.description) != 0:
+            self.add_description_fields(condition.description)
